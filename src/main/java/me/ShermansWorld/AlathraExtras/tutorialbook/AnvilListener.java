4770a0d39c6a6a74714a021a4516e91b8a5ec337
Xavier Beatrice <58835993+Xavbeat03@users.noreply.github.com>
2025-03-24 18:38:14 -0400

    fix: dupe

--- src/main/java/me/ShermansWorld/AlathraExtras/tutorialbook/AnvilListener.java
+++ src/main/java/me/ShermansWorld/AlathraExtras/tutorialbook/AnvilListener.java
@@ -3,10 +3,12 @@ package me.ShermansWorld.AlathraExtras.tutorialbook;
 import me.ShermansWorld.AlathraExtras.Helper;
 import me.ShermansWorld.AlathraExtras.items.Items;
 
+import org.bukkit.Material;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.inventory.PrepareAnvilEvent;
+import org.bukkit.inventory.ItemStack;
 
 public class AnvilListener implements Listener {
     @EventHandler
@@ -17,6 +19,10 @@ public class AnvilListener implements Listener {
                 p.closeInventory();
                 p.sendMessage(Helper.Chatlabel() + Helper.color("&cYou cannot put the Player's Guide in an anvil!"));
 
+            } else if (e.getInventory().getItem(0).getType() == Material.SHULKER_BOX || e.getInventory().getItem(1).getType() == Material.SHULKER_BOX) {
+                Player p = (Player) e.getView().getPlayer();
+                p.closeInventory();
+                p.sendMessage(Helper.Chatlabel() + Helper.color("&cYou cannot put a shulker box in an anvil!"));
             }
         }
     }
