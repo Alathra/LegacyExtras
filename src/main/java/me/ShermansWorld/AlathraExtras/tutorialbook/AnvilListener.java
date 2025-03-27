a6a86595b2c17a187e0db5f89d828bfd83e27456
Xavier Beatrice <58835993+Xavbeat03@users.noreply.github.com>
2025-03-26 21:30:09 -0400

    fix: dupe (#142)

--- src/main/java/me/ShermansWorld/AlathraExtras/tutorialbook/AnvilListener.java
+++ src/main/java/me/ShermansWorld/AlathraExtras/tutorialbook/AnvilListener.java
@@ -1,5 +1,6 @@
 package me.ShermansWorld.AlathraExtras.tutorialbook;
 
+import com.destroystokyo.paper.MaterialTags;
 import me.ShermansWorld.AlathraExtras.Helper;
 import me.ShermansWorld.AlathraExtras.items.Items;
 
@@ -10,17 +11,22 @@ import org.bukkit.event.Listener;
 import org.bukkit.event.inventory.PrepareAnvilEvent;
 import org.bukkit.inventory.ItemStack;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class AnvilListener implements Listener {
     @EventHandler
     public void commandSent(PrepareAnvilEvent e) {
+        List<Material> shulkerBoxMaterials = Arrays.asList(Material.SHULKER_BOX, Material.BLACK_SHULKER_BOX, Material.BLUE_SHULKER_BOX, Material.BROWN_SHULKER_BOX, Material.CYAN_SHULKER_BOX, Material.GRAY_SHULKER_BOX, Material.GREEN_SHULKER_BOX, Material.LIGHT_BLUE_SHULKER_BOX, Material.LIGHT_GRAY_SHULKER_BOX, Material.LIME_SHULKER_BOX, Material.MAGENTA_SHULKER_BOX, Material.ORANGE_SHULKER_BOX, Material.PINK_SHULKER_BOX, Material.PURPLE_SHULKER_BOX, Material.RED_SHULKER_BOX, Material.WHITE_SHULKER_BOX, Material.YELLOW_SHULKER_BOX);
+        Player p = (Player) e.getView().getPlayer();
         if (e.getInventory().getItem(0) != null) {
             if (e.getInventory().getItem(0).isSimilar(Items.tutorialBook())) {
-                Player p = (Player) e.getView().getPlayer();
                 p.closeInventory();
                 p.sendMessage(Helper.Chatlabel() + Helper.color("&cYou cannot put the Player's Guide in an anvil!"));
 
-            } else if (e.getInventory().getItem(0).getType() == Material.SHULKER_BOX || e.getInventory().getItem(1).getType() == Material.SHULKER_BOX) {
-                Player p = (Player) e.getView().getPlayer();
+            } else if (shulkerBoxMaterials.contains(e.getInventory().getItem(0).getType()) || shulkerBoxMaterials.contains(e.getInventory().getItem(1).getType())) {
+                e.setResult(new ItemStack(Material.AIR));
+
                 p.closeInventory();
                 p.sendMessage(Helper.Chatlabel() + Helper.color("&cYou cannot put a shulker box in an anvil!"));
             }
